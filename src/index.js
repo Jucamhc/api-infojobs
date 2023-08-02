0bec4b5baefb6f3160a4ef1c229203db9a06f575
Kamilo2251 <camilo2251@gmail.com>
2023-08-02 17:52:27 -0500

    sta

--- src/index.js
+++ src/index.js
@@ -95,7 +95,7 @@ app.get('/api-offers/search', async (req, res) => {
 
         const response = await fetch(`https://www.infojobs.net/webapp/offers/search?keyword=${keyword}&normalizedJobTitleIds=${normalizedJobTitleIds}&provinceIds=${provinceIds}&cityIds=${cityIds}&teleworkingIds=${teleworkingIds}&categoryIds=${categoryIds}&workdayIds=${workdayIds}&educationIds=${educationIds}&segmentId=${segmentId}&contractTypeIds=${contractTypeIds}&page=${page}&sortBy=${sortBy}&onlyForeignCountry=${onlyForeignCountry}&countryIds=${countryIds}&sinceDate=${sinceDate}&subcategoryIds=${subcategoryIds}`, requestOptions);
 
-        console.log(response);
+        console.log(response.status);
 
         if (!response.ok) {
             throw new Error('Error en la petición');
